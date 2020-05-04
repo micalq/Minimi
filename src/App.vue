d1d6380d810fc6fbbfb9ef112a5851ed2f201557
陈政 <1427270056@qq.com>
2020-05-04 20:12:45 +0800

    mock模拟数据

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,24 @@
 <script>
 import storage from "./storage/index"//导入storage
 export default {
+  data() {
+    return {
+
+      res:{}
+    }
+  },
       mounted() {
         // storage.setItem("a",1)
         // storage.setItem("q",{qq:11},"user")
         // storage.clear("a")
         // storage.clear("q","user")
+       /*  this.axios.get("/mock/user/login.json").then((res)=>{ //第一种方法mock本地加载请求
+          this.res=res
+        }) */
+        this.axios.get("/user/login").then((res)=>{
+          this.res=res
+          console.log(res);
+        })
       },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import env from './env'
+// import env from './env'
 Vue.config.productionTip = false  //开发模式默认关闭
-// Vue.use(axios,VueAxios)//安装其他插件的时候，可以直接在 main.js 中引入并使用 Vue.use()来注册，但是 axios并不是vue插件，所以不能 使用Vue.use()，所以只能在每个需要发送请求的组件中即时引入。
-Vue.prototype.axios = axios
-Vue.prototype.VueAxios=VueAxios
+Vue.use(VueAxios,axios)//VueAxios前面 axios依赖VueAxios，安装其他插件的时候，可以直接在 main.js 中引入并使用 Vue.use()来注册，但是 axios并不是vue插件，所以不能 使用Vue.use()，所以只能在每个需要发送请求的组件中即时引入。
+// Vue.prototype.axios = axios
+// Vue.prototype.VueAxios=VueAxios
 // axios.defaults.baseURL="http://test-www.imooc.com/api"//默认请求地址
-axios.defaults.baseURL=env.baseURL//默认请求地址
+// axios.defaults.baseURL=env.baseURL//默认请求地址
+// axios.defaults.baseURL=" https://www.easy-mock.com/mock/5eb00394a9af2438fc69f153/api"//easymock
 // console.log(axios.defaults.baseURL);
-// axios.defaults.baseURL="/api"//默认请求地址
+const mock=false; //mock开关
+if (mock) {
+    require("./mock/api")
+}
+axios.defaults.baseURL="/api"//默认请求地址
 axios.defaults.timeout=8000//默认超时时间
 axios.interceptors.response.use((response)=>{//接口错误拦截
   let res=response.data;
